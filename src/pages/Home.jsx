ea9162ce6acd6db8a0ec3f53ee39f41396fb73e3
Wadad Parker <wadadparker5@gmail.com>
2023-06-14 20:51:45 +0530

    feat: profile card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import { ProfileCard } from "../components/profileCard/ProfileCard.jsx"
+
 export const Home=()=>
 {
     return (
         <div>
+            <ProfileCard />
             <h1>Hello, absolute paths are working!!</h1>
         </div>
     )
