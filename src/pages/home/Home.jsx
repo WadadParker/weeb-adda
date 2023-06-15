ad53b62be1fdb3b6cae876a691310b0db73e08be
Wadad Parker <wadadparker5@gmail.com>
2023-06-15 20:46:27 +0530

    style: search bar

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import {Sidebar} from "src/components/sidebar/sidebar.jsx";
 import {PostCard} from "src/components/postcard/PostCard.jsx";
 import { FilterBar } from "src/components/filterbar/FilterBar";
+import { SearchBar } from "src/components/searchbar/SearchBar";
 
 import styles from "src/pages/home/home.module.css";
 
@@ -14,6 +15,7 @@ export const Home=()=>
                     <li className={styles[`post-list-item`]}><PostCard /></li>
                     <li className={styles[`post-list-item`]}><PostCard /></li>
                 </ul>
+            <SearchBar />    
         </div>
     )
 }
