77bf340ca4e79353785aae2a0c3eb46ad62d774d
Wadad Parker <wadadparker5@gmail.com>
2023-06-18 02:02:58 +0530

    feat: css modules

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import styles from "src/pages/home.module.css";
+
 export const Home=()=>
 {
     return (
         <div>
-            <h1>Hello, absolute paths are working!!</h1>
+            <h1><span className={styles.hello}>Hello</span>, absolute paths are working!!</h1>
+            <h2 className={styles.heading}>CSS modules is also installed!</h2>
         </div>
     )
 }
