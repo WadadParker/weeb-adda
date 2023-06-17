0fa11a8a1d738e2f65288e8cbf42ee6362c300cc
Wadad Parker <wadadparker5@gmail.com>
2023-06-18 01:57:40 +0530

    feat: absolute imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import {Home} from "src/pages/Home"
+
 if (import.meta.env.DEV) {
 	window.onerror = (event, source, lineno, colno, err) => {
 		const ErrorOverlay = customElements.get("vite-error-overlay");
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// absolute import
 export default defineConfig({
   plugins: [react()],
   resolve: {
