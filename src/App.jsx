07ab22a81545b90adb24301ce683d28ad79fd4bd
Wadad Parker <wadadparker5@gmail.com>
2023-06-21 11:20:34 +0530

    style: login page

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import {Route,Routes} from "react-router-dom";
 import {Home} from "src/pages/home/Home.jsx";
 import { Post } from "src/pages/post/Post";
 import { Profile } from "src/pages/profile/Profile";
-import { LandingPage } from "src/pages/landingPage/LandingPage";
+import { LoginPage } from "src/pages/login/LoginPage.jsx";
 
 if (import.meta.env.DEV) {
 	window.onerror = (event, source, lineno, colno, err) => {
@@ -25,7 +25,7 @@ function App() {
 		<>
 			
 				<Routes>
-					<Route path="/" element={<LandingPage />} />
+					<Route path="/" element={<LoginPage />} />
 					<Route path="/home" element={<Home />} />
 					<Route path="/post/:postID" element={<Post />} />
 					<Route path="/profile/:username" element={<Profile />} />
---
+++ src/pages/login/LoginPage.jsx
@@ -0,0 +1,28 @@
+import styles from "./loginPage.module.css";
+
+import logo from "src/assets/background.jpg";
+
+export const LoginPage=()=>
+{
+    return (
+        <div className={styles[`landing-page-container`]}>
+            
+            <header className={styles.header}>
+                <h1 style={{fontSize:"7rem",margin:"0"}}>Weeb Adda</h1>
+                <main className={styles[`login-container`]}>
+                    <h2>Login</h2>
+                    <label htmlFor="email">Enter Email</label>
+                    <input id="label"></input>
+
+                    <label htmlFor="password">Enter Password</label>
+                    <input type="password" id="password"></input>
+                    <button>Login</button>
+                    <button>Enter Guest Credentials</button>
+
+                    <a href="/signup">Create new account</a>
+                </main>
+            </header>
+            <img alt="" src={logo} className={styles.img}/>
+        </div>
+    )
+}
