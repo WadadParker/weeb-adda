d7caa56cb32d27a8e01214ce32280ef80f146674
Wadad Parker <wadadparker5@gmail.com>
2023-06-22 22:28:03 +0530

    style: Edit form Design

--- src/components/editUser/EditUser.jsx
+++ src/components/editUser/EditUser.jsx
@@ -7,7 +7,7 @@ import { ProfileContext } from "src/context/ProfileContext";
 
 export const EditUser=()=>
 {
-    const {showModal,dispatch}=useContext(ProfileContext);
+    const {dispatch}=useContext(ProfileContext);
     return (
         <div className={styles.modal}>
             <div className={styles[`form-container`]}>
@@ -16,9 +16,28 @@ export const EditUser=()=>
                     <h2>Edit Profile</h2>
                     <button onClick={()=>dispatch({type:"TOGGLE_MODAL",payload:false})}>Submit</button>
                 </header>
-                <main>
-                <img className={styles.coverimg} alt="" src="https://i.pinimg.com/originals/07/ae/16/07ae16b891cd997f2e2f45e597a52ef5.jpg"  height={300}/>
+                <img className={styles.coverimg} alt="" src="https://i.pinimg.com/originals/07/ae/16/07ae16b891cd997f2e2f45e597a52ef5.jpg"  height={200}/>
+                <img className={styles.pfp} alt="" src="https://i.pinimg.com/originals/98/c0/83/98c08367933f07c451a18c4507f011a5.jpg" width={100} height={100}/>
+                <h2>Choose Avatar</h2>
+                <div className={styles[`avatar-container`]}>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349118.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280968.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280958.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349122.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280960.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280967.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280970.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349119.png" width={100} height={100} className={styles.avatar}/>
+                </div>
+                <br />
+                <main className={styles.main}>
+                    <label>Bio</label>
+                    <input className={styles.bio}></input>
                 </main>
+                <footer className={styles.main}>
+                    <label>Website</label>
+                    <input className={styles.bio}></input>
+                </footer>
             </div>
         </div>
     )
