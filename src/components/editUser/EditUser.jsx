8fde2e17dc6b3103a81b9d51fa79d3820c2a7a66
Wadad Parker <wadadparker5@gmail.com>
2023-06-22 22:45:46 +0530

    refactor: avatar DB

--- src/components/editUser/EditUser.jsx
+++ src/components/editUser/EditUser.jsx
@@ -4,9 +4,11 @@ import { useContext } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 import { ProfileContext } from "src/context/ProfileContext";
+import { Avatars } from "src/assets/avatars/Avatars";
 
 export const EditUser=()=>
 {
+    const {avatar1,avatar2,avatar3,avatar4,avatar5,avatar6,avatar7,avatar8}=Avatars;
     const {dispatch}=useContext(ProfileContext);
     return (
         <div className={styles.modal}>
@@ -17,17 +19,17 @@ export const EditUser=()=>
                     <button onClick={()=>dispatch({type:"TOGGLE_MODAL",payload:false})}>Submit</button>
                 </header>
                 <img className={styles.coverimg} alt="" src="https://i.pinimg.com/originals/07/ae/16/07ae16b891cd997f2e2f45e597a52ef5.jpg"  height={200}/>
-                <img className={styles.pfp} alt="" src="https://i.pinimg.com/originals/98/c0/83/98c08367933f07c451a18c4507f011a5.jpg" width={100} height={100}/>
-                <h2>Choose Avatar</h2>
+                <img className={styles.pfp} alt="" src="https://i.pinimg.com/originals/98/c0/83/98c08367933f07c451a18c4507f011a5.jpg" width={120} height={120}/>
+                <h2 className={styles.heading}>Choose Avatar</h2>
                 <div className={styles[`avatar-container`]}>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349118.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280968.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280958.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349122.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280960.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280967.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/280/thumb-280970.png" width={100} height={100} className={styles.avatar}/>
-                    <img alt="" src="https://avatarfiles.alphacoders.com/349/thumb-349119.png" width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar1} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar2} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar3} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar4} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar5} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar6} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar7} width={100} height={100} className={styles.avatar}/>
+                    <img alt="" src={avatar8} width={100} height={100} className={styles.avatar}/>
                 </div>
                 <br />
                 <main className={styles.main}>
