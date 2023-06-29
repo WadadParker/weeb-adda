266bc42094252576337d980d7697920fec8059b3
Wadad Parker <wadadparker5@gmail.com>
2023-06-29 05:30:52 +0530

    feat: explore page

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -1,14 +1,33 @@
-import styles from "./explore.module.css";
+import styles from "src/pages/home/home.module.css";
+import { useContext } from "react";
+import { PostContext } from "src/context/PostContext";
+import { ProfileContext } from "src/context/ProfileContext";
 
 import {Sidebar} from "src/components/sidebar/sidebar.jsx";
 import { SearchBar } from "src/components/searchbar/SearchBar";
+import { FilterBar } from "src/components/filterbar/FilterBar";
+import {PostCard} from "src/components/postcard/PostCard.jsx";
 
 export const Explore=()=>
 {
+    const {state:{userPosts,content}}=useContext(PostContext);
+    const {state:{currentUser}}=useContext(ProfileContext);
     return (
-        <div>
+        <div className={styles[`home-container`]}>
             <Sidebar />
-            This is Explore page
+            <ul className={styles[`posts-list-container`]}> 
+                <FilterBar />
+                    {userPosts?.map(item=>{
+                        const {_id}=item;
+                        return (
+                            <li key={_id} className={styles[`post-list-item`]}>
+                                {item?.username===currentUser?.username
+                                ?<PostCard post={item} isCurrentUser={true}/>
+                                :<PostCard post={item} username={item?.username} isCurrentUser={false}/>}
+                            </li>
+                        )
+                    })}
+                </ul>
             <SearchBar />
         </div>
     )
