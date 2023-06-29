ca70cf978f877579a7e3e88059ff80b6992a7543
Wadad Parker <wadadparker5@gmail.com>
2023-06-30 05:12:02 +0530

    feat: search users

--- src/components/searchbar/SearchBar.jsx
+++ src/components/searchbar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import styles from "src/components/searchbar/searchBar.module.css"
 
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { PostContext } from "src/context/PostContext";
 import { ProfileContext } from "src/context/ProfileContext";
 
@@ -9,14 +10,21 @@ import {FollowerCard} from "src/components/followerCard/FollowerCard.jsx";
 export const SearchBar=()=>
 {
     const {state:{allUsers}}=useContext(PostContext);
-    const {state:{currentUser}}=useContext(ProfileContext);
+    const {state:{currentUser,search},dispatch}=useContext(ProfileContext);
+    const navigate=useNavigate();
 
     const suggestedUsers = allUsers.filter(user => !currentUser?.following.some(followedUser => followedUser.username === user.username || user.username===currentUser?.username));
+    const searchedUsers=allUsers.filter(({name})=>name.toLowerCase().includes(search.toLowerCase()));
 
+    const clickHandler=(username)=>
+    {
+        navigate(`/profile/${username}`)
+        dispatch({type:"UPDATE_SEARCH",payload:""});
+    }
 
     return (
         <div className={styles[`search-users-container`]}>
-            <input type="search" placeholder="search users" className={styles.input}/>
+            <input type="search" placeholder="search users" value={search} className={styles.input} onChange={(e)=>dispatch({type:"UPDATE_SEARCH",payload:e.target.value})}/>
             <ul className={styles[`suggested-users-list`]}>
                 {suggestedUsers?.map((user,index)=>{
                     const {_id}=user;
@@ -26,6 +34,17 @@ export const SearchBar=()=>
                     )
                 })}
             </ul>
+            {search.length!==0 && <div className={styles[`searched-users-container`]}>
+                {searchedUsers?.map(user=>{
+                    return (
+                        <li key={user?._id} className={styles[`searched-user-list-item`]}>
+                            <img alt="pfp" src={user?.avatar} width={80} height={80} className={styles.pfp}/>
+                            <strong onClick={()=>clickHandler(user?.username)}>{user?.name}</strong>
+                            <span>@{user?.username}</span>
+                        </li>
+                    )
+                })}
+            </div>}
         </div>
     )
 }
