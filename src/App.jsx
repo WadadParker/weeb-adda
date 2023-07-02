5e71fa96f6eca58e381f7d071f0ad07bfac6cabf
Wadad Parker <wadadparker5@gmail.com>
2023-07-02 22:50:40 +0530

    feat: css module app file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+import styles from "src/app.module.css";
+
 
 if (import.meta.env.DEV) {
 	window.onerror = (event, source, lineno, colno, err) => {
@@ -28,7 +30,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>Vite + React</h1>
+			<h1 className={styles.header}>Vite + React</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
